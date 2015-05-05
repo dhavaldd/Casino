8bf2282bf9b014b1eab0eae140f42feca4a03068
Dhaval <damnniwala.dhaval@gmail.com>
2015-05-05 18:36:47 -0400

    Server Update

--- Casino/src/ClientServer/Server.java
+++ Casino/src/ClientServer/Server.java
@@ -31,19 +31,7 @@ public class Server {
      * spawns handler threads.
      */
     public static void main(String[] args) throws Exception {
-    	try {
-            // Assume default encoding.
-			PrintWriter writer = new PrintWriter("Players.txt");
-			writer.print("0");
-			writer.close();
-        }
-        catch(IOException ex) {
-            System.out.println(
-                "Error writing to file '"
-                + "Players.txt" + "'");
-            // Or we could just do this:
-            // ex.printStackTrace();
-        }
+    	
         System.out.println("The chat server is running.");
         ServerSocket listener = new ServerSocket(PORT);
         try {
